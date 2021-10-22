4a613912e9a8e9a7f0a9ce15542d2e5735258e67
Limas Jaya Akeh <limas.akeh@binus.edu>
2021-10-23 01:10:54 +0700

    feat: added Home screen and jump from home to quiz

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,12 +1,66 @@
-import React ,{useState }from 'react'
-import { View, SafeAreaView, Text, StyleSheet,Pressable } from 'react-native'
-import { useDispatch } from 'react-redux'
+import React ,{useEffect, useState }from 'react'
+import { Button, View, SafeAreaView, Text, TextInput,Dimensions, StyleSheet,Pressable } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
 import {setScore} from '../Store/Reducers/ScoreReducer'
+import { profileUserName, setUserName } from '../Store/Reducers/ProfileReducer'
 import ModalComponent from '../components/molecules/Modal'
-const HomeScreen = () => {
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import RouteName from '../Routes/RouterName'
+const vw = Dimensions.get('window').width;
+const vh = Dimensions.get('window').height;
+
+const Stack = createNativeStackNavigator()
+const RealHomeScreen = ({navigation}) => {
+    const [userName, setThisUserName] = useState('No Name')
     const [currentScore, setCurrentScore] = useState(0);
     const [modalVisible, setModalVisible] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("")
+
+
+    const profile_userName = useSelector(profileUserName)
+    useEffect(() => {
+        console.log("effect triggered")
+        setThisUserName(profile_userName)
+        return () => console.log("effect unmounted")
+    }, [profile_userName])
     const dispatch = useDispatch();
+
+    const textInputHandler = (inputValue) => {
+        if(inputValue.length < 4){
+            setThisUserName(inputValue)
+            setErrorMessage("User Name is too short")
+            return
+        }
+        else if(inputValue.length > 20){
+            setThisUserName(inputValue)
+            setErrorMessage("User Name is too long")
+            return
+        }
+        else{
+            setThisUserName(inputValue)
+            setErrorMessage("")
+            dispatch(setUserName(inputValue))
+            return
+        }
+    }
+    const endTextInputHandler = () => {
+        let inputValue = userName
+        if(inputValue.length < 4){
+            setThisUserName(profile_userName)
+            setErrorMessage("")
+            return
+        }
+        else if(inputValue.length > 20){
+            setThisUserName(profile_userName)
+            setErrorMessage("")
+            return
+        }
+    }
+
+    const handleClick = () => {
+        console.log('Clicked')
+        navigation.navigate(RouteName.Quiz)
+    }
 
     const setScoreHome = (value) =>{
         dispatch(setScore(value))
@@ -22,8 +76,24 @@ const HomeScreen = () => {
                 >
                     <Text style={styles.textStyle}>Show Modal</Text>
                 </Pressable>
+
+                <View style={[styles.fill]}>
+                    <Text style={[styles.defaultText]}>Name:</Text>
+                        <TextInput style={[styles.input]} value={userName} onChangeText={textInputHandler} onBlur={endTextInputHandler}/>
+                    <Text>{errorMessage}</Text> 
+                    <Button style={[styles.button]} title={'Start Quiz'} onPress={handleClick}/>
+                </View>
+
             </View>
         </SafeAreaView>
+    )
+}
+
+const HomeScreen = ({navigation}) => {
+    return (
+        <Stack.Navigator initialRouteName="RealHome" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="RealHome" component={RealHomeScreen} prop={navigation} />
+        </Stack.Navigator>
     )
 }
 
@@ -51,7 +121,15 @@ const styles = StyleSheet.create({
         color: "white",
         fontWeight: "bold",
         textAlign: "center"
-    }
+    },
+    input: {
+        height: 40,
+        borderWidth: 1,
+        padding: 10,
+        width: 0.7*vw,
+        borderRadius:8,
+        marginBottom:0.03*vh
+    },
 })
 
 export default HomeScreen
